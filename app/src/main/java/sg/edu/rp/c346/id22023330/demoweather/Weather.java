d73fbb82e3769582b66624705df99bc4b998a8a4
22023330 <22023330@myrp.edu.sg>
2023-08-21 10:07:49 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22023330/demoweather/Weather.java
@@ -0,0 +1,33 @@
+package sg.edu.rp.c346.id22023330.demoweather;
+
+public class Weather {
+
+        private String area;
+        private String forecast;
+
+        public Weather(String area, String forecast) {
+            this.area = area;
+            this.forecast = forecast;
+        }
+
+        public String getArea() {
+            return area;
+        }
+
+        public String getForecast() {
+            return forecast;
+        }
+
+        public void setArea(String area) {
+            this.area = area;
+        }
+
+        public void setForecast(String forecast) {
+            this.forecast = forecast;
+        }
+
+        public String toString() {
+            return "Area: " + area + "\nForecast: " + forecast;
+        }
+
+}
